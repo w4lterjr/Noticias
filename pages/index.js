a755675aeb9da4f453b967f73fa41ef420dc4f41
Walter Junior <75254804+w4lterjr@users.noreply.github.com>
2024-11-14 13:19:53 +0000

    test

--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,7 @@ export async function getServerSideProps() {
         country: 'us',
       },
     });
+    //
     articles = res.data.articles || [];
   } catch (err) {
     console.error('Error fetching news articles:', err);
