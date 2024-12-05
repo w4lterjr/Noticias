5da8010ac93ae365835d27f887ed6ef108f7cf6a
Walter Junior <75254804+w4lterjr@users.noreply.github.com>
2024-12-05 15:09:00 +0000

    futebol_2

--- components/Futebol.js
+++ components/Futebol.js
@@ -4,6 +4,8 @@ import styles from '../styles/Futebol.module.css';
 
 const BrasileiraoClassificacao = () => {
   const [classificacao, setClassificacao] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchClassificacao = async () => {
@@ -15,8 +17,10 @@ const BrasileiraoClassificacao = () => {
           }
         );
         setClassificacao(response.data.standings[0].table);
+        setLoading(false);
       } catch (error) {
-        console.error("Erro ao buscar a classificação", error);
+        setError("Erro ao buscar a classificação");
+        setLoading(false);
       }
     };
 
@@ -26,13 +30,23 @@ const BrasileiraoClassificacao = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.h2}>Classificação do Brasileirão</h2>
-      <ul className={styles.ul}>
-        {classificacao.map((time) => (
-          <li key={time.team.id} className={styles.li}>
-            {time.position} - {time.team.name} ({time.points} pontos)
-          </li>
-        ))}
-      </ul>
+
+      {/* Se estiver carregando, exibe uma mensagem de "Carregando..." */}
+      {loading && <div className={styles.loading}>Carregando...</div>}
+
+      {/* Se houver erro, exibe a mensagem de erro */}
+      {error && <div className={styles.error}>{error}</div>}
+
+      {/* Se não houver erro, exibe a classificação */}
+      {!loading && !error && (
+        <ul className={styles.ul}>
+          {classificacao.map((time) => (
+            <li key={time.team.id} className={styles.li}>
+              {time.position} - {time.team.name} ({time.points} pontos)
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
