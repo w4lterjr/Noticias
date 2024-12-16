42fbe7ebfd5d7453fca131774c98791e4ee4bb95
Walter Junior <75254804+w4lterjr@users.noreply.github.com>
2024-12-16 12:51:19 +0000

    cotacoes

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,6 @@ export default function Home({ articles, error }) {
           <Weather />
           <br/>
           <Ibovespa />
-          <br/>
           <Cambio />
           <br/>
           <Futebol />
